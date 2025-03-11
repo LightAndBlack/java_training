c2f7e0fc9d2ec6469ff4642f735afcd54f7a8aad
One <pavel.svitsenka@gmail.com>
2025-03-11 03:49:06 +0300

    4 добавил файл

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ public class Main{
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main( String[] args ) {
         logger.setLevel(Level.INFO);
-//        Action action = new Action();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
